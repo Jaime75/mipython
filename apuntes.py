704271c3806224b1a623e349461efce02382815c
Jaime Esq <jaimeesqueda@gmail.com>
2022-07-06 15:17:07 -0600

    primer dia de apuntes python

---
+++ apuntes.py
@@ -0,0 +1,8 @@
+#print('Hola munto') # esto imprime en pantalla lo que esta entre parentesis
+#x = 5 # se crea la variable x, ademas se le asigna el valor de 5
+
+#nombre = 'Lucas' # se crea la variable nombre y se le asigna la cadena Lucas
+#edad = 20
+#print('Hola mundo, soy ' + nombre  + ' y tengo ' + str(edad) + ' años ') #ejemplo de uso
+
+
