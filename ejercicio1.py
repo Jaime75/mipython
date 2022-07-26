632c1a4198addee04e6a21cbe94475eed584accd
Jaime Esqueda <jaimeesqueda@gmail.com>
2022-07-26 16:49:47 -0600

    Se agrega calculo de promedio al programa

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,13 @@
-pregunta1 = "¿Qué desea hacer?"
-textoInicial = 'Para calcula distancia presione "a" '
+def calcularPromedio(n1, n2, n3):
+    promedio = (n1 + n2 + n3) / 3
+    return promedio
+
+def pedirDatosPromedio():
+    n1 = float(input('Cual es la primer nota: \n'))
+    n2 = float(input('Cual es la segunda nota: \n'))
+    n3 = float(input('Cual es la tercer nota: \n'))
+    promedio = calcularPromedio(n1, n2, n3)
+    print('El promedio de las 3 notas es: \n' + str(promedio))
 
 def calcularDistancia(velocidad, tiempo):
     distancia = velocidad * tiempo
@@ -11,4 +19,21 @@ def pedirDatos():
     distancia = calcularDistancia(velocidad, tiempo)
     print('La distancia recorrida por el móvil es: '+ str(distancia))
 
-pedirDatos()
+
+def textos():
+    print('Para calcular la distancia presiona "a" ')
+    print('Para calcular el promedio de 3 notas presiona "b" ')
+    print('Para salir presiona "x"')
+
+textos()
+
+respuestaPregunta = input('Que desea hacer?: \n')
+
+if respuestaPregunta == 'a':
+    pedirDatos()
+
+if respuestaPregunta == 'b':
+    pedirDatosPromedio()
+
+if respuestaPregunta == 'x':
+    print('Hasta pronto')
