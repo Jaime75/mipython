5171f637144b64e4524b8c82af650288fc25f66f
Jaime Esq <jaimeesqueda@gmail.com>
2022-08-03 22:09:54 -0600

    agregando calculo de area y perimetro de un rectangulo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,33 @@
+
+from cmath import sqrt
+
+def calcularPerimetroRectangulo(base, altura):
+    perimetro = base * 2 + altura * 2
+    return perimetro
+
+def calcularAreaRectangulo(base, altura):
+    area = base * altura
+    return area
+
+def pedirDatosRectangulo():
+    base = float(input('Cual es la medida de la base del rectángulo?: \n'))
+    altura = float(input('Cual es la altura del rectángulo?: \n'))
+    perimetro = calcularPerimetroRectangulo(base, altura)
+    area = calcularAreaRectangulo(base, altura)
+    print('El perimetro del rectángulo es: ' + str(perimetro) + ' el área del rectángulo es: ' + str(area))
+
+def calcularDistanciaDosPuntos(x1, y1, x2, y2):
+    distancia = sqrt((x2-x1)**2 +(y2 - y1)**2)
+    return distancia
+
+def pedirCoordenadas():
+    x1 = float(input('Introduzca el valor de la abscisa del primer punto: '))
+    y1 = float(input('Introduzca el valor de la ordenada del primer punto: '))
+    x2 = float(input('Introduzca el valor de la abscisa del segundo punto: '))
+    y2 = float(input('Introduzca el valor de la ordenada del segundo punto: '))
+    distancia = calcularDistanciaDosPuntos(x1, y1, x2, y2)
+    print('La distancia es: ' + str(distancia))
+
 def calcularPuntaje(correctas, incorrectas, blanco):
     puntaje = (correctas * 3 + incorrectas * (-1) + blanco * 0)
     return puntaje
@@ -35,6 +65,8 @@ def textos():
     print('Para calcular la distancia presiona "a" ')
     print('Para calcular el promedio de 3 notas presiona "b" ')
     print('Para calcular el puntaje presiona "c" ')
+    print('Para calcular la distancia entre 2 puntos presiona "d"')
+    print('Para calcular el perimetro y el área de un rectángulo presione "e"')
     print('Para salir presiona "x"')
 
 
@@ -53,6 +85,14 @@ def pregunta():
     
     if respuestaPregunta == 'c':
         pedirDatosPuntaje()
+        pregunta()
+
+    if respuestaPregunta == 'd':
+        pedirCoordenadas()
+        pregunta()
+
+    if respuestaPregunta == 'e':
+        pedirDatosRectangulo()
         pregunta()
 
     if respuestaPregunta == 'x':
